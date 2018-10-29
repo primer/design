08b2f6e173319464ac8744c072dfe97e08125286
Shawn Allen <shawn.allen@github.com>
2018-10-29 13:43:49 -0700

    refactor SideNav with pageMap

--- src/SideNav.js
+++ src/SideNav.js
@@ -4,27 +4,6 @@ import {default as NextLink} from 'next/link'
 import {Text, BorderBox, Box, Link, Flex, Relative} from '@primer/components'
 import {pageMap} from './utils'
 
-const getLink = router => {
-  return Object.values(pageMap).map(({displayName: name}) => {
-    const isSelected = router.pathname === `/components/docs/${name}`
-    return (
-      <Box mb={3} key={name}>
-        <NextLink href={`/components/docs/${name}`}>
-          <Link href={`/components/docs/${name}`} color={isSelected ? 'gray.9' : 'blue.5'} fontSize={1}>
-            {name}
-          </Link>
-        </NextLink>
-      </Box>
-    )
-  })
-}
-
-const isComponentLink = componentName => {
-  return Object.values(pageMap)
-    .map(n => n.displayName)
-    .includes(componentName)
-}
-
 const Section = ({children, ...rest}) => (
   <BorderBox {...rest}>
     <Flex flexDirection="column" alignItems="start">
@@ -35,6 +14,7 @@ const Section = ({children, ...rest}) => (
 
 Section.defaultProps = {
   p: 5,
+  pb: 2,
   border: 0,
   borderBottom: 1,
   borderColor: 'gray.2',
@@ -42,58 +22,48 @@ Section.defaultProps = {
   bg: null
 }
 
-const SideNav = ({router}) => (
-  <Relative>
+const SideNav = ({router, ...rest}) => (
+  <Relative {...rest}>
     <BorderBox
+      id="sidenav"
       width={['100%', '100%', 256, 256]}
       height="100%"
       bg="gray.0"
-      border={0}
+      borderLeft={0}
+      borderBottom={0}
       borderRight={1}
       borderTop={[1, 1, 0, 0]}
       borderColor="gray.2"
       borderRadius={0}
-      id="sidenav"
     >
       <Section>
-        <NextLink href="/components/docs/system-props">
-          <Link
-            color="gray.9"
-            href="/components/docs/system-props"
-            m={0}
-            mb={4}
-            fontWeight={router.pathname === '/components/docs/system-props' ? 'bold' : null}
-          >
-            System Props
-          </Link>
-        </NextLink>
-        <NextLink href="/components/docs/primer-theme">
-          <Link
-            color="gray.9"
-            href="/components/docs/primer-theme"
-            m={0}
-            fontWeight={router.pathname === '/components/docs/primer-theme' ? 'bold' : null}
-          >
-            Primer Theme
-          </Link>
-        </NextLink>
+        <SectionLink href="/components/docs/system-props">System Props</SectionLink>
+        <SectionLink href="/components/docs/primer-theme">Primer Theme</SectionLink>
       </Section>
-      <Box pt={5} pl={5}>
-        <Text is="p" color="black" m={0} mb={3}>
-          <NextLink href="/components/docs/Avatar">
-            <Link
-              color="gray.9"
-              href="/components/docs/Avatar"
-              fontWeight={isComponentLink(router.pathname.replace('/components/docs/', '')) ? 'bold' : null}
-            >
-              Components
-            </Link>
-          </NextLink>
-        </Text>
-        {getLink(router)}
-      </Box>
+      <Section>
+        <SectionLink href="/components/docs/Avatar">Components</SectionLink>
+        {[...Object.entries(pageMap)].map(([path, file]) => (
+          <SideLink href={path} key={path}>{file}</SideLink>
+        ))}
+      </Section>
     </BorderBox>
   </Relative>
 )
+
+const SectionLink = withRouter(({href, router, ...rest}) => (
+  <Box mb={3}>
+    <NextLink href={href}>
+      <Link href={href} m={0} color="gray.9" fontWeight={router.pathname === href ? 'bold' : null} {...rest} />
+    </NextLink>
+  </Box>
+))
+
+const SideLink = withRouter(({href, router, ...rest}) => (
+  <Box mb={3}>
+    <NextLink href={href}>
+      <Link href={href} color={router.pathname === href ? 'gray.9' : 'blue.5'} fontSize={1} {...rest} />
+    </NextLink>
+  </Box>
+))
 
 export default withRouter(SideNav)
