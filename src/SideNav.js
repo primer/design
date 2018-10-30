afcec5b77c13c71282d3599629a850c0436894e3
Shawn Allen <shawn.allen@github.com>
2018-10-30 16:56:13 -0700

    refactor SideNav to build from pageTree

--- src/SideNav.js
+++ src/SideNav.js
@@ -2,65 +2,65 @@ import React from 'react'
 import {withRouter} from 'next/router'
 import {default as NextLink} from 'next/link'
 import {BorderBox, Box, Link, Flex, Relative} from '@primer/components'
-import {pageMap} from './utils'
+import {getDisplayName, pageTree} from './utils'
 
-const Section = ({children, ...rest}) => (
-  <BorderBox {...rest}>
-    <Flex flexDirection="column" alignItems="start">
-      {children}
-    </Flex>
-  </BorderBox>
-)
+const requirePage = require.context('../pages', true, /\.(js|md)x?$/)
 
-Section.defaultProps = {
-  p: 5,
-  pb: 2,
-  border: 0,
-  borderBottom: 1,
-  borderColor: 'gray.2',
-  borderRadius: 0,
-  bg: null
+export default function SideNav(props) {
+  return (
+    <Relative {...props}>
+      <BorderBox
+        id="sidenav"
+        width={['100%', '100%', 256, 256]}
+        height="100%"
+        bg="gray.0"
+        borderLeft={0}
+        borderBottom={0}
+        borderRight={1}
+        borderTop={[1, 1, 0, 0]}
+        borderColor="gray.2"
+        borderRadius={0}
+      >
+        {pageTree.children.map(node => <Section key={node.path} node={node} />)}
+      </BorderBox>
+    </Relative>
+  )
 }
 
-const SideNav = props => (
-  <Relative {...props}>
+function Section({node, ...rest}) {
+  const {path, file, children} = node
+  return (
     <BorderBox
-      id="sidenav"
-      width={['100%', '100%', 256, 256]}
-      height="100%"
-      bg="gray.0"
-      borderLeft={0}
-      borderBottom={0}
-      borderRight={1}
-      borderTop={[1, 1, 0, 0]}
+      px={5}
+      py={2}
+      border={0}
+      borderBottom={1}
       borderColor="gray.2"
       borderRadius={0}
+      bg={null}
+      {...rest}
     >
-      <Section>
-        <SectionLink href="/components/docs/system-props">System Props</SectionLink>
-        <SectionLink href="/components/docs/primer-theme">Primer Theme</SectionLink>
-      </Section>
-      <Section>
-        <SectionLink href="/components/docs/Avatar">Components</SectionLink>
-        {[...Object.entries(pageMap)].map(([path, file]) => (
-          <SideLink href={path} key={path}>
-            {file}
-          </SideLink>
+      <Flex flexDirection="column" alignItems="start">
+        <SectionLink href={path}>{getNavName(file)}</SectionLink>
+        {children.map(child => (
+          <PageLink key={child.path} href={child.path}>
+            {getNavName(child.file)}
+          </PageLink>
         ))}
-      </Section>
+      </Flex>
     </BorderBox>
-  </Relative>
-)
+  )
+}
 
 const SectionLink = withRouter(({href, router, ...rest}) => (
-  <Box mb={3}>
+  <Box my={3}>
     <NextLink href={href}>
       <Link href={href} color="gray.9" fontWeight={router.pathname === href ? 'bold' : null} {...rest} />
     </NextLink>
   </Box>
 ))
 
-const SideLink = withRouter(({href, router, ...rest}) => (
+const PageLink = withRouter(({href, router, ...rest}) => (
   <Box mb={3}>
     <NextLink href={href}>
       <Link href={href} color={router.pathname === href ? 'gray.9' : 'blue.5'} fontSize={1} {...rest} />
@@ -68,4 +68,15 @@ const SideLink = withRouter(({href, router, ...rest}) => (
   </Box>
 ))
 
-export default SideNav
+function getNavName(file) {
+  const page = requirePage.keys().find(key => key === `.${file}`)
+  let name
+  if (page) {
+    const Component = requirePage(page)
+    if (!Component) {
+      throw new Error(`Nothing is exported by ${file}! Did you add "export const meta {displayName: 'Blah'}"?`)
+    }
+    name = getDisplayName(Component)
+  }
+  return name || file
+}
