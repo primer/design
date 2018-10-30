154c9e6f24ee4fe2c7f6b2609da8699cbf580e23
Shawn Allen <shawn.allen@github.com>
2018-10-30 16:55:30 -0700

    add pageTree, getDisplayName() utils

--- src/utils.js
+++ src/utils.js
@@ -6,7 +6,12 @@ export const assetPrefix = config.assetPrefix || ''
 export const assetPath = `${assetPrefix}/static/assets/`
 
 export const pageMap = config.pageMap || {}
+export const pageTree = config.pageTree || {}
 
 export function getAssetPath(path) {
   return `${assetPath}${path}`
+}
+
+export function getDisplayName(obj) {
+  return (obj.meta ? obj.meta.displayName : null) || obj.displayName || obj.name
 }
