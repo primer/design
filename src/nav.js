ffc513f31f44c9873db49e6793a782e2ef8d5441
Shawn Allen <shawn.allen@github.com>
2018-10-31 00:18:23 -0700

    move nav-specific stuff out of utils

---
+++ src/nav.js
@@ -0,0 +1,30 @@
+import {config} from './utils'
+
+export const pageMap = config.pageMap || {}
+export const pageTree = config.pageTree || {}
+
+export const ROOT_URL = pageTree.path || '/'
+
+export const requirePage = require.context('../pages', true, /\.(js|md)x?$/)
+
+export function getComponent(file) {
+  const page = requirePage.keys().find(key => key === `.${file}`)
+  if (page) {
+    return requirePage(page)
+  } else {
+    console.warn(`no file to require from file: ${file}`)
+  }
+}
+
+export function getNavName(file) {
+  let name
+  const Component = getComponent(file)
+  if (Component) {
+    name = getDisplayName(Component)
+  }
+  return name || file
+}
+
+export function getDisplayName(obj) {
+  return (obj.meta ? obj.meta.displayName : null) || obj.displayName || obj.name
+}
--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,10 @@
 import getConfig from 'next/config'
 
-const config = getConfig().publicRuntimeConfig || {}
+export const config = getConfig().publicRuntimeConfig || {}
 
 export const assetPrefix = config.assetPrefix || ''
 export const assetPath = `${assetPrefix}/static/assets/`
 
-export const pageMap = config.pageMap || {}
-export const pageTree = config.pageTree || {}
-
 export function getAssetPath(path) {
   return `${assetPath}${path}`
-}
-
-export function getDisplayName(obj) {
-  return (obj.meta ? obj.meta.displayName : null) || obj.displayName || obj.name
 }
