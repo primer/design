5723b7ea6579eff4306662a418381819041c857a
Shawn Allen <shawn.allen@github.com>
2018-10-31 00:21:01 -0700

    refactor Header to use pageTree

--- src/Header.js
+++ src/Header.js
@@ -4,13 +4,19 @@ import {withRouter} from 'next/router'
 import Octicon, {MarkGithub} from '@githubprimer/octicons-react'
 import {Text, Flex, Link, Sticky, BorderBox, Box} from '@primer/components'
 import BoxShadow from './BoxShadow'
+import {getComponent, getNavName, pageTree, ROOT_URL} from './nav'
 
-const Header = ({router}) => (
-  <Sticky zIndex={100}>
+const links = pageTree.children.filter(({file}) => {
+  const Component = getComponent(file)
+  return Component && Component.meta && Component.meta.nav === 'top'
+})
+
+const Header = props => (
+  <Sticky zIndex={100} {...props}>
     <BoxShadow py={3} bg="gray.9" color="white">
       <Flex px={[3, 6, 6, 3]} alignItems="center" justifyContent="space-between">
-        <NextLink href="/components">
-          <Link ml={3} color="white" href="/components">
+        <NextLink href={ROOT_URL}>
+          <Link color="white" href={ROOT_URL} ml={3}>
             <Flex alignItems="center" justifyContent="center">
               <Octicon icon={MarkGithub} size="medium" />
               <Text ml={3}>Primer Design Guidelines</Text>
@@ -18,7 +24,7 @@ const Header = ({router}) => (
           </Link>
         </NextLink>
         <Box display={['none', 'none', 'block']}>
-          <Links router={router} />
+          <Links />
         </Box>
         <Box display={['block', 'block', 'none']}>
           <Link href="#sidenav">
@@ -43,15 +49,20 @@ const Header = ({router}) => (
   </Sticky>
 )
 
-const links = [{text: 'Docs', href: '/components'}, {text: 'Sandbox', href: '/components/sandbox'}]
+function Links(props) {
+  return links.map(link => <PageLink key={link.path} link={link} {...props} />)
+}
 
-const Links = ({router}) =>
-  links.map(({href, text}) => (
-    <NextLink href={href} key={href}>
-      <Link color="white" href={href} px={4} fontWeight={router.pathname === href ? 'bold' : null}>
-        {text}
+const PageLink = withRouter(({link, router, ...rest}) => {
+  const {path, file} = link
+  const current = router.pathname === path
+  return (
+    <NextLink to={path}>
+      <Link href={path} color="white" ml={4} fontWeight={current ? 'bold' : 'normal'}>
+        {getNavName(file)}
       </Link>
     </NextLink>
-  ))
+  )
+})
 
-export default withRouter(Header)
+export default Header
