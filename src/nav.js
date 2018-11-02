3ada11e20e5f9dee454c009169afb87f172fbcf8
Shawn Allen <shawn.allen@github.com>
2018-11-02 15:37:00 -0700

    refactor nav utils as a TreeModel

--- src/nav.js
+++ src/nav.js
@@ -1,61 +1,30 @@
+import TreeModel from 'tree-model'
 import {config} from './utils'
 
-export const pageMap = config.pageMap || {}
-export const pageTree = config.pageTree || {}
+const pageTree = config.pageTree || {}
 
-// augment the page tree with a function that can find
-// a node recursively given a test function
-pageTree.find = check => {
-  function findIn(node) {
-    return check(node) || node.children.find(findIn)
-  }
-  return findIn(pageTree)
-}
+const tree = new TreeModel()
+const root = tree.parse(pageTree)
 
-export const ROOT_URL = pageTree.path || '/'
+export default root
 
-const requirePage = require.context('../pages', true, /\.(js|md)x?$/)
+export function populateTree(context) {
+  const keys = context.keys()
+  root.walk(node => {
+    const {path, file} = node.model
+    const key = keys.find(key => key === `.${file}`)
+    const component = key ? context(key) : {}
+    const meta = component.meta || {}
+    const name = meta.displayName || component.displayName || component.name || path
+    Object.assign(node, {component, meta, name, path})
+  })
 
-export function getComponent(file) {
-  const page = requirePage.keys().find(key => key === `.${file}`)
-  if (page) {
-    return requirePage(page)
-  } else {
-    // eslint-disable-next-line no-console
-    console.warn(`no file to require from file: ${file}`)
-  }
-}
+  // sort all of the child lists by name or path
+  const compareKey = key => (a, b) => a[key].localeCompare(b[key])
+  const compareName = compareKey('name')
+  const comparePath = compareKey('path')
+  const cmpNode = (a, b) => compareName(a, b) || comparePath(a, b)
+  root.walk(node => node.children.sort(cmpNode))
 
-export function getNavName(file) {
-  let name
-  const Component = getComponent(file)
-  if (Component) {
-    name = getDisplayName(Component)
-  }
-  return name || file
-}
-
-export function isFileHidden(file) {
-  return getFileMeta(file, 'hidden') === true
-}
-
-export function isComponentHidden(Component) {
-  return getComponentMeta(Component, 'hidden') === true
-}
-
-export function getFileMeta(file, key = null) {
-  const Component = getComponent(file)
-  if (Component) {
-    return getComponentMeta(Component, key)
-  }
-  return undefined
-}
-
-export function getComponentMeta(Component, key = null) {
-  const meta = Component.meta || {}
-  return key ? meta[key] : meta
-}
-
-export function getDisplayName(obj) {
-  return (obj.meta ? obj.meta.displayName : null) || obj.displayName || obj.name
+  return root
 }
