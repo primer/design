08a4877f92986ba44611535effbd2127369df26a
Joshua Shao <jhuashao@github.com>
2018-11-08 13:55:44 -0800

    Revert "Scaffold the site"

--- pages/index.js
+++
@@ -1,3 +0,0 @@
-import {redirect} from '../src/utils'
-export default redirect('/design')
-export const meta = {redirect: true}
--- src/constants.js
+++
@@ -1,2 +0,0 @@
-export const SITE_TITLE = 'Primer Design'
-export const ROOT_URL = '/design'
