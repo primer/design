0c32a661859394e170a8ed434812457341c1a7be
Emily <emplums@github.com>
2019-02-27 09:42:13 -0800

    fix heading font weight

--- src/IndexHero.js
+++ src/IndexHero.js
@@ -6,7 +6,7 @@ const IndexHero = () => (
   <Box bg="black">
     <Box maxWidth={1012} py={6} mb={3} mx="auto" px={6}>
       <Box mt={4} mb={4}>
-        <Heading color="blue.4" fontSize={7} pb={3}>
+        <Heading color="blue.4" fontWeight='bold' fontSize={7} pb={3}>
           Primer Design
         </Heading>
       </Box>
