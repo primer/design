90544e0879595a7e592b1ef235c659851684894a
Mike Perrotti <mperrotti@github.com>
2022-05-26 17:00:46 -0400

    adds custom video player

---
+++ src/CustomVideoPlayer.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import {Box} from '@primer/components'
+import {ColumnsIcon, PlayIcon} from '@primer/octicons-react'
+
+const CustomVideoPlayer = (props: React.HTMLProps<HTMLVideoElement>) => {
+  const videoElement = React.useRef<HTMLVideoElement | null>(null)
+  const [isPlaying, setIsPlaying] = React.useState<boolean>(props.autoPlay)
+
+  const playVideo = () => {
+    videoElement.current.play()
+    setIsPlaying(true)
+  }
+
+  const pauseVideo = () => {
+    videoElement.current.pause()
+    setIsPlaying(false)
+  }
+
+  return (
+    <Box
+      position="relative"
+      sx={{
+        ':hover button': {
+          opacity: 1,
+        },
+      }}
+    >
+      {/* component users would be able to pass their own <track> children */}
+      {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
+      <video {...props} ref={videoElement} />
+      {!props.controls && (
+        <Box
+          as="button"
+          position="absolute"
+          sx={{
+            backgroundColor: 'rgba(0,0,0,0.55)',
+            border: 'none',
+            cursor: 'pointer',
+            inset: '0px',
+            opacity: isPlaying ? 0 : 1,
+          }}
+          onClick={isPlaying ? pauseVideo : playVideo}
+        >
+          {isPlaying ? <ColumnsIcon size={40} fill="white" /> : <PlayIcon size={50} fill="white" />}
+        </Box>
+      )}
+    </Box>
+  )
+}
+
+export default CustomVideoPlayer
