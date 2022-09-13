a0e787d8d224392b44bfd2116c10fbd125f85e99
langermank <langermank@github.com>
2022-09-13 10:24:41 -0700

    setup underline nav

---
+++ src/StoryMenu.js
@@ -0,0 +1,16 @@
+// export function submitted() {
+//   link = document.getElementById('select-story').value
+//   document.getElementById('storybook-preview-iframe').src = link + '&shortcuts=false&singleStory=true&showPanel=false'
+// }
+
+// ;<form onSubmit="return false;">
+//   <select id="select-story">
+//     <option value="" selected>
+//       Pick a Key and Peele youtube video
+//     </option>
+//     <option value="pSDTmJtE-Bc">Little Homie</option>
+//     <option value="Dd7FixvoKBw">Substitute Teacher</option>
+//     <option value="iGAMbNKcN1U">Fronthand Backhand</option>
+//   </select>
+//   <button onClick={() => submitted()}>Submit</button>
+// </form>
