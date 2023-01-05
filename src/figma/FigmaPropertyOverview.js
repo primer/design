b8802e5a35af7e19a668567dacceaf6b325e8b64
Lukas Oppermann <lukasoppermann@github.com>
2023-01-05 10:30:19 +0100

    style fixes

--- src/figma/FigmaPropertyOverview.js
+++ src/figma/FigmaPropertyOverview.js
@@ -1,8 +1,42 @@
 import React from 'react'
-import {Heading, Box, Text, StyledOcticon} from '@primer/react'
+import {Heading, Box, Text, StyledOcticon, Link} from '@primer/react'
 import Table from '@primer/gatsby-theme-doctocat/src/components/table'
 import InlineCode from '@primer/gatsby-theme-doctocat/src/components/inline-code'
-import {IssueReopenedIcon} from '@primer/octicons-react'
+import {SyncIcon, TypographyIcon, DiamondIcon} from '@primer/octicons-react'
+
+function PropertyValues({property}) {
+  let values = property.values.map((value, index) => (
+    <>
+      <InlineCode key={index}>{value}</InlineCode> 
+    </>
+  ))
+
+  if (property.type === "TEXT") {
+    values = (
+      <InlineCode style={{padding: '0.3em 0.4em', verticalAlign: 'middle'}}>
+        <StyledOcticon icon={TypographyIcon} sx={{mr: 2}} verticalAlign="middle" />
+        text
+      </InlineCode>
+    )
+  }
+
+  if (property.type === 'INSTANCE_SWAP') {
+    values = (
+      <InlineCode style={{padding: '0.3em 0.4em', verticalAlign: 'middle'}}>
+        <Link sx={{color: 'fg.default'}} hoverColor='accent.fg' href="https://help.figma.com/hc/en-us/articles/360039150413-Swap-components-and-instances">
+          <StyledOcticon icon={SyncIcon} sx={{mr: 2}} verticalAlign="middle" />
+          instance swap
+        </Link>
+      </InlineCode>
+    )
+  }
+
+  return (
+    <>
+      {values}
+    </>
+  )
+}
 
 export default function FigmaPropertyOverview({properties}) {
   const combinationCount = properties
@@ -19,7 +53,7 @@ export default function FigmaPropertyOverview({properties}) {
           marginBottom: 4
         }}
       >
-        <StyledOcticon icon={IssueReopenedIcon} sx={{mr: 2}} />
+        <StyledOcticon icon={DiamondIcon} sx={{mr: 2}} />
         <Text fontWeight={'bold'}>{combinationCount}</Text>
         <Text>&nbsp;</Text>
         <Text>variants</Text>
@@ -39,18 +73,7 @@ export default function FigmaPropertyOverview({properties}) {
               <tr key={index}>
                 <td>{property.name}</td>
                 <td>
-                  <Box
-                    display="flex"
-                    flexWrap={'wrap'}
-                    sx={{
-                      alignItems: 'start',
-                      gap: 2
-                    }}
-                  >
-                    {property.values.map((value, index) => (
-                      <InlineCode key={index}>{value}</InlineCode>
-                    ))}
-                  </Box>
+                  <PropertyValues property={property} />
                 </td>
                 <td>
                   <InlineCode>{property.defaultValue}</InlineCode>
