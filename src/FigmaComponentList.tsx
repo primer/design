bc2bca686d25ae3d539b0ab804d4a1d247b4f35a
Josep Martins <josepmartins@github.com>
2023-02-02 14:05:08 +0100

    Move figma files to /guides/figma

--- src/FigmaComponentList.tsx
+++ src/FigmaComponentList.tsx
@@ -1,15 +1,16 @@
-import { Box, Link, Text } from '@primer/react'
-import { useStaticQuery, graphql } from "gatsby"
+import {Box, Link, Text} from '@primer/react'
+import {useStaticQuery, graphql} from 'gatsby'
 import React from 'react'
 
-
 export default function FigmaComponentList() {
-  const {allMdx: {edges: components}} = useStaticQuery(graphql`
+  const {
+    allMdx: {edges: components}
+  } = useStaticQuery(graphql`
     query {
       allMdx(
-        filter: {fileAbsolutePath: {glob: "**/content/figma/components/*"}, frontmatter: {draft: {ne: true}}}
+        filter: {fileAbsolutePath: {glob: "**/content/guides/figma/components/*"}, frontmatter: {draft: {ne: true}}}
         sort: {fields: frontmatter___title}
-        ) {
+      ) {
         edges {
           node {
             id
@@ -28,10 +29,16 @@ export default function FigmaComponentList() {
   return (
     <Box>
       {components.map(component => {
-        return (<Box key={component.node.slug}>
-          <Link sx={{ fontWeight: 'bold' }} href={`/${component.node.slug}`}>{component.node.frontmatter.title}</Link>
-          <Text as="p" sx={{marginTop: '0'}}>{component.node.frontmatter.description}</Text>
-        </Box>)
+        return (
+          <Box key={component.node.slug}>
+            <Link sx={{fontWeight: 'bold'}} href={`/${component.node.slug}`}>
+              {component.node.frontmatter.title}
+            </Link>
+            <Text as="p" sx={{marginTop: '0'}}>
+              {component.node.frontmatter.description}
+            </Text>
+          </Box>
+        )
       })}
     </Box>
   )
