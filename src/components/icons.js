8345cb8ea2205db79664837db317ae2c96892528
Cole Bemis <colebemis@github.com>
2023-02-07 13:19:53 -0700

    Update headings

--- src/components/icons.js
+++ src/components/icons.js
@@ -1,36 +1,42 @@
-import {Box, Heading, Link, TextInput} from '@primer/react'
+import {H2} from '@primer/gatsby-theme-doctocat/src/components/heading'
 import {SearchIcon} from '@primer/octicons-react'
+import icons from '@primer/octicons-react/build/data.json'
+import {Box, Link, TextInput} from '@primer/react'
 import {Link as GatsbyLink} from 'gatsby'
 import React from 'react'
-import icons from '@primer/octicons-react/build/data.json'
-import useSearch from './use-search'
 import Icon from './icon'
+import useSearch from './use-search'
 
 export default function Icons() {
   const [query, setQuery] = React.useState('')
   const iconsArray = React.useMemo(() => {
-    return Object.values(icons).flatMap(icon => Object.entries(icon.heights).map(([height, value]) => ({
+    return Object.values(icons).flatMap(icon =>
+      Object.entries(icon.heights).map(([height, value]) => ({
         name: icon.name,
         keywords: icon.keywords,
         width: value.width,
         height,
-        path: value.path
-      }))
+        path: value.path,
+      })),
     )
   }, [icons])
   const results = useSearch(iconsArray, query, {keys: ['name', 'keywords']})
   // group icons by height property
-  const iconsByHeight = React.useMemo(() => results.reduce((acc, cur) => {
-    // create height array if it doesn't exist
-    if (!acc[cur.height]) acc[cur.height] = []
-    // add current icon to height array
-    acc[cur.height].push(cur)
+  const iconsByHeight = React.useMemo(
+    () =>
+      results.reduce((acc, cur) => {
+        // create height array if it doesn't exist
+        if (!acc[cur.height]) acc[cur.height] = []
+        // add current icon to height array
+        acc[cur.height].push(cur)
 
-    return acc
-  }, {}), [results])
+        return acc
+      }, {}),
+    [results],
+  )
 
   return (
-    <Box display={"grid"} gridGap={5}>
+    <Box display="grid" gridGap={5}>
       <TextInput
         leadingVisual={SearchIcon}
         aria-label="Search"
@@ -42,11 +48,11 @@ export default function Icons() {
       {Object.entries(iconsByHeight).length > 0
         ? Object.entries(iconsByHeight).map(([height, icons]) => (
             <Box key={height}>
-              <Heading as="h2" fontSize={3} mb={3}>
+              <H2>
                 {height}
                 px
-              </Heading>
-              <Box display={"flex"} flexWrap="wrap" mx={-3}>
+              </H2>
+              <Box display="flex" flexWrap="wrap" mx={-3}>
                 {icons.map(icon => (
                   <Link
                     as={GatsbyLink}
@@ -54,11 +60,11 @@ export default function Icons() {
                     sx={{
                       display: 'block',
                       padding: 3,
-                      color: 'inherit'
+                      color: 'inherit',
                     }}
                     to={`${icon.name}-${icon.height}`}
                   >
-                    <Box display={"flex"}>
+                    <Box display="flex">
                       <Icon width={icon.width} height={icon.height} path={icon.path} />
                     </Box>
                   </Link>
