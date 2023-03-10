9a26b4fb762308f59af259cd165293bdfdf9441e
Cole Bemis <colebemis@github.com>
2023-03-10 04:01:34 -0700

    Show source code in React component examples (#412)
    * Add the ability to show source code in storybook embed
    
    * Ignore ts error
    
    * Show react story source code
    
    * Update gatsby-node.js
    
    * Use main component.json file

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,6 @@
 const path = require('path')
 const defines = require('./babel-defines')
 const fetch = require('node-fetch')
-const {paramCase} = require('change-case')
 
 exports.onCreateWebpackConfig = ({actions, plugins, getConfig}) => {
   const config = getConfig()
@@ -66,22 +65,6 @@ async function sourcePrimerReactData({actions, createNodeId, createContentDigest
       internal: {
         type: 'ReactComponent',
         contentDigest: createContentDigest({...component, componentId: component.id}),
-      },
-    }
-
-    actions.createNode(newNode)
-  }
-
-  // Save Primer React storybook data to the GraphQL store
-  const storiesJson = await fetch(`https://primer.style/react/storybook/stories.json`).then(res => res.json())
-
-  for (const story of Object.values(storiesJson.stories)) {
-    const newNode = {
-      ...{...story, storyId: story.id},
-      id: createNodeId(`react-${story.id}`),
-      internal: {
-        type: 'ReactComponentStory',
-        contentDigest: createContentDigest({...story, storyId: story.id}),
       },
     }
 
