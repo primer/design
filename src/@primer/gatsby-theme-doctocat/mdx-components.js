6504cb9b91842cb47f6341e873c875b4fc21463f
Cole Bemis <colebemis@github.com>
2023-03-17 14:58:14 -0600

    Create ChildPages component

--- src/@primer/gatsby-theme-doctocat/mdx-components.js
+++ src/@primer/gatsby-theme-doctocat/mdx-components.js
@@ -1,5 +1,6 @@
 import CustomVideoPlayer from '../../CustomVideoPlayer'
 import ImageBox from './components/ImageBox'
 import StorybookEmbed from '../../components/storybook-embed'
+import {ChildPages} from '../../components/child-pages'
 
-export default {CustomVideoPlayer, ImageBox, StorybookEmbed}
+export default {CustomVideoPlayer, ImageBox, StorybookEmbed, ChildPages}
