6b9778769166a85735341eb25d10222b73bf62f3
Josep Martins <josepmartins@github.com>
2023-07-18 17:07:20 +0200

    Style changes

--- src/components/member-questions.js
+++ src/components/member-questions.js
@@ -17,40 +17,37 @@ const MemberMarkdown = ({source, colorName}) => {
 const MemberInfo = ({member, colorName}) => {
   const color = colorName === 'orange' ? 'severe.fg' : 'accent.fg'
   return (
-    <Box width={[1, 1, 1, 1 / 2]} id={member.handle}>
-      <Heading sx={{color: color}} fontSize={5} pb={3} lineHeight={1.25}>
-        {member.name}, {member.title}
-      </Heading>
-      <Link
-        sx={{mt: 2, fontFamily: 'mono', color: color, hoverColor: color, fontSize: 2}}
-        href={`https://github.com/${member.handle}`}
-      >
-        <StyledOcticon icon={MarkGithubIcon} size={24} sx={{mr: 3, verticalAlign: 'middle'}} />@{member.handle}
+    <Box width={[1, 1, 1, 1]} id={member.handle}>
+      <Heading>{member.name}</Heading>
+      <Text sx={{color: 'fg.subtle'}}>{member.title}</Text>
+      {' • '}
+      <Link sx={{mt: 2, color: color, hoverColor: color, fontSize: 2}} href={`https://github.com/${member.handle}`}>
+        @{member.handle}
       </Link>
-      <Text fontFamily="mono" color={color} as="div" fontSize={3} mt={7} mb={0}>
+      <Text as="p" fontSize={3} fontWeight="bold" mt={7} mb={0}>
         What drew you into design systems?
       </Text>
-      <Text fontSize={3}>
+      <Text fontSize={3} mt={0}>
         <MemberMarkdown source={member.questionOne} colorName={color} />
       </Text>
-      <Text fontFamily="mono" color={color} as="div" fontSize={3} mt={7} mb={0}>
+      <Text as="p" fontSize={3} fontWeight="bold" mt={7} mb={0}>
         Who have you learned from or been inspired by?
       </Text>
-      <Text fontSize={3}>
+      <Text fontSize={3} mt={0}>
         <MemberMarkdown source={member.questionTwo} colorName={color} />
       </Text>
-      <Text fontFamily="mono" color={color} as="div" fontSize={3} mt={7} mb={0}>
+      <Text as="p" fontSize={3} fontWeight="bold" mt={7} mb={0}>
         Favorite tools
       </Text>
-      <Text fontSize={3}>
+      <Text fontSize={3} mt={0}>
         <MemberMarkdown source={member.favoriteTools} colorName={color} />
       </Text>
       {member.cssFeature ? (
         <>
-          <Text fontFamily="mono" color={color} as="div" fontSize={3} mt={7} mb={0}>
+          <Text as="p" fontSize={3} fontWeight="bold" mt={7} mb={0}>
             Favorite CSS feature
           </Text>
-          <Text color={`${colorName}.1`} fontSize={3}>
+          <Text color={`${colorName}.1`} fontSize={3} mt={0}>
             <MemberMarkdown source={member.cssFeature} colorName={color} />
           </Text>
         </>
--- src/layouts/minimal-layout.tsx
+++ src/layouts/minimal-layout.tsx
@@ -11,7 +11,9 @@ export default function MinimalLayout({pageContext, children, path}) {
   return (
     <BaseLayout showSidebar={false} title={title} description={description}>
       <Box sx={{maxWidth: 960, width: '100%', p: [4, 5, 6, 7], mx: 'auto'}}>
-        <Heading as="h1">{title}</Heading>
+        <Heading as="h1" sx={{fontSize: 7, mb: 2}}>
+          {title}
+        </Heading>
         {description ? (
           <Text as="p" sx={{fontSize: 3, m: 0, mb: 3, maxWidth: '60ch'}}>
             {description}
