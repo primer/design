1b61d9cc981662486cc033b167904088d93a99c0
Josep Martins <josepmartins@github.com>
2023-07-18 17:47:28 +0200

    Adjust parapgraph styles in markdown

--- src/components/member-questions.js
+++ src/components/member-questions.js
@@ -9,40 +9,38 @@ const MemberMarkdown = ({source, colorName}) => {
     </Link>
   )
 
+  const paragraph = props => (
+    <Text as="p" fontSize={3} mt={1} mb={3} {...props}>
+      {props.children}
+    </Text>
+  )
+
   const trimmedSource = source.replace(/\n */g, '\n')
-  return <ReactMarkdown className="markdown" children={trimmedSource} components={{a: link}} />
+  return <ReactMarkdown className="markdown" children={trimmedSource} components={{a: link, p: paragraph}} />
 }
 
 const MemberInfo = ({member, colorName}) => {
   const color = colorName === 'orange' ? 'severe.fg' : 'accent.fg'
   return (
-    <Box width={[1, 1, 1, 1]} id={member.handle} mt={5}>
+    <Box id={member.handle} mt={5}>
       <Text as="p" fontSize={3} fontWeight="bold" mt={7} mb={0}>
         What drew you into design systems?
       </Text>
-      <Text fontSize={3} mt={0}>
-        <MemberMarkdown source={member.questionOne} colorName={color} />
-      </Text>
+      <MemberMarkdown source={member.questionOne} colorName={color} />
       <Text as="p" fontSize={3} fontWeight="bold" mt={7} mb={0}>
         Who have you learned from or been inspired by?
       </Text>
-      <Text fontSize={3} mt={0}>
-        <MemberMarkdown source={member.questionTwo} colorName={color} />
-      </Text>
+      <MemberMarkdown source={member.questionTwo} colorName={color} />
       <Text as="p" fontSize={3} fontWeight="bold" mt={7} mb={0}>
         Favorite tools
       </Text>
-      <Text fontSize={3} mt={0}>
-        <MemberMarkdown source={member.favoriteTools} colorName={color} />
-      </Text>
+      <MemberMarkdown source={member.favoriteTools} colorName={color} />
       {member.cssFeature ? (
         <>
           <Text as="p" fontSize={3} fontWeight="bold" mt={7} mb={0}>
             Favorite CSS feature
           </Text>
-          <Text color={`${colorName}.1`} fontSize={3} mt={0}>
-            <MemberMarkdown source={member.cssFeature} colorName={color} />
-          </Text>
+          <MemberMarkdown source={member.cssFeature} colorName={color} />
         </>
       ) : null}
     </Box>
