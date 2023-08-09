d28e4d9f1be48992c8ab982b00b926860611e924
Emily Brick <brick.emily@gmail.com>
2023-08-08 21:25:57 -0400

    added max-width to minimal layout

--- src/layouts/minimal-layout.tsx
+++ src/layouts/minimal-layout.tsx
@@ -10,7 +10,7 @@ export default function MinimalLayout({pageContext, children, path}) {
 
   return (
     <BaseLayout showSidebar={false} title={title} description={description}>
-      <Box sx={{maxWidth: 960, width: '100%', p: [4, 5, 6, 7], mx: 'auto'}}>
+      <Box sx={{maxWidth: 1200, width: '100%', p: [4, 5, 6, 7], mx: 'auto'}}>
         <Heading as="h1" sx={{fontSize: 7}}>
           {title}
         </Heading>
