8c74c4819644ca865db57fa749b40319d61e38c2
Emily Brick <brick.emily@gmail.com>
2023-09-07 10:10:50 -0400

    updated accessibility component

--- src/components/accessibility-link.tsx
+++ src/components/accessibility-link.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {IssueOpenedIcon} from '@primer/octicons-react'
+import {Link} from '@primer/react'
 
 export function AccessibilityLink({label}){
-    return <><IssueOpenedIcon /> <a target="_blank" href={`https://github.com/github/primer/issues?q=is%3Aopen+is%3Aissue+label%3A%22component%3A+${label}%22+label%3Aaccessibility`}> View open accessibility issues related to this component</a></>
+    return <><IssueOpenedIcon /><Link href={`https://github.com/github/primer/issues?q=is%3Aopen+is%3Aissue+label%3A%22component%3A+${label}%22+label%3Aaccessibility`}> View open accessibility issues related to this component</Link></>
 }
