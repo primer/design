62899bb5696a2db10c1c55cd6939f5bb4ab81c64
Emily Brick <brick.emily@gmail.com>
2023-09-07 10:25:13 -0400

    updated Link to match link in footer

--- src/components/accessibility-link.tsx
+++ src/components/accessibility-link.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import {IssueOpenedIcon} from '@primer/octicons-react'
-import {Link} from '@primer/react'
+import {Link, StyledOcticon} from '@primer/react'
 
 export function AccessibilityLink({label}){
-    return <><IssueOpenedIcon /><Link href={`https://github.com/github/primer/issues?q=is%3Aopen+is%3Aissue+label%3A%22component%3A+${label}%22+label%3Aaccessibility`}> View open accessibility issues related to this component</Link></>
+    return (
+        <Link href={`https://github.com/github/primer/issues?q=is%3Aopen+is%3Aissue+label%3A%22component%3A+${label}%22+label%3Aaccessibility`}>
+            <StyledOcticon icon={IssueOpenedIcon} sx={{mr: 2}} />
+            View open accessibility issues related to this component
+        </Link>
+    )
 }
