629d0040eb88120de370bbb4074935d5e15be8d9
Cameron Dutro <camertron@gmail.com>
2024-01-29 14:56:58 -0800

    Fix dialog props

--- src/components/react-component-page.tsx
+++ src/components/react-component-page.tsx
@@ -2,17 +2,15 @@ import {AccessibilityLabel, Note, StatusLabel} from '@primer/gatsby-theme-doctoc
 import Code from '@primer/gatsby-theme-doctocat/src/components/code'
 import {HEADER_HEIGHT} from '@primer/gatsby-theme-doctocat/src/components/header'
 import {H2, H3} from '@primer/gatsby-theme-doctocat/src/components/heading'
-import InlineCode from '@primer/gatsby-theme-doctocat/src/components/inline-code'
-import Table from '@primer/gatsby-theme-doctocat/src/components/table'
 import TableOfContents from '@primer/gatsby-theme-doctocat/src/components/table-of-contents'
 import {LinkExternalIcon} from '@primer/octicons-react'
 import {Box, Heading, Label, Link, Text} from '@primer/react'
 import React, { PropsWithChildren } from 'react'
-import ReactMarkdown from 'react-markdown'
 import {StorybookEmbed} from '../components/storybook-embed'
 import {BaseLayout} from '../components/base-layout'
 import {ComponentPageNav} from '../components/component-page-nav'
 import StatusMenu from '../components/status-menu'
+import ReactPropsTable from './react-props-table'
 
 type ReactComponentPageProps = {
   // Data is the result of the ReactComponentInfo graphql fragment in react-component-layout.tsx
@@ -239,77 +237,4 @@ function sentenceCase(str: string) {
   return str.replace(/([A-Z])/g, ' $1').replace(/^./, function (str) {
     return str.toUpperCase()
   })
-}
-
-// TODO: Make table responsive
-function ReactPropsTable({
-  props,
-}: {
-  props: Array<{
-    name: string
-    type: string
-    defaultValue: string
-    required: boolean
-    deprecated: boolean
-    description: string
-  }>
-}) {
-  if (props.length === 0) {
-    return (
-      <Box sx={{padding: 3, bg: 'canvas.inset', textAlign: 'center', color: 'fg.muted', borderRadius: 2}}>No props</Box>
-    )
-  }
-
-  return (
-    <Box sx={{overflow: 'auto'}}>
-      <Table>
-        <colgroup>
-          <col style={{width: '25%'}} />
-          <col style={{width: '15%'}} />
-          <col style={{width: '60%'}} />
-        </colgroup>
-        <thead>
-          <tr>
-            <th align="left">Name</th>
-            <th align="left">Default</th>
-            <th align="left">Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.map(prop => (
-            <tr key={prop.name}>
-              <td valign="top">
-                <Box sx={{display: 'flex', gap: 2, alignItems: 'center'}}>
-                  <Text sx={{fontFamily: 'mono', fontSize: 1, whiteSpace: 'nowrap'}}>{prop.name}</Text>
-                  {prop.required ? <Label>Required</Label> : null}
-                  {prop.deprecated ? <Label variant="danger">Deprecated</Label> : null}
-                </Box>
-              </td>
-              <td valign="top">{prop.defaultValue ? <InlineCode>{prop.defaultValue}</InlineCode> : null}</td>
-              <td>
-                <InlineCode>{prop.type}</InlineCode>
-                <Box
-                  sx={{
-                    '&:not(:empty)': {
-                      mt: 2,
-                    },
-                    color: 'fg.muted',
-                    '& > :first-child': {
-                      mt: 0,
-                    },
-                    '& > :last-child': {
-                      mb: 0,
-                    },
-                  }}
-                >
-                  {/* @ts-ignore */}
-                  <ReactMarkdown components={{a: Link, code: InlineCode}}>{prop.description}</ReactMarkdown>
-                </Box>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </Box>
-  )
 }
