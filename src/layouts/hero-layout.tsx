f18d27f653d4f5ec56a38dfd28df80ec72464bc4
Daniel Adams <dipree@github.com>
2024-04-23 19:22:27 +0200

    Remove obsolete from hero layout

--- src/layouts/hero-layout.tsx
+++ src/layouts/hero-layout.tsx
@@ -14,32 +14,6 @@ type NavItemData = {
 export default function ComponentLayout({pageContext, children, path}) {
   const {title, description, reactId, railsIds, figmaId, cssId} = pageContext.frontmatter
 
-  const getPageAncestry = (url: string, object: NavItemData[]) => {
-    const result: NavItemData[] = []
-    const buildArray = (node: NavItemData, path: string) => {
-      if (node.url === path) {
-        result.push({title: node.title, url: node.url})
-      } else if (node.children) {
-        for (const child of node.children) {
-          buildArray(child, path)
-          if (result.length > 0) {
-            result.unshift({title: node.title, url: node.url})
-            break
-          }
-        }
-      }
-    }
-    for (const node of object) {
-      buildArray(node, url)
-      if (result.length > 0) {
-        break
-      }
-    }
-    return result
-  }
-
-  const breadcrumbData = getPageAncestry(path, navItems).filter(item => item.url)
-
   return (
     <BaseLayout title={title} description={description}>
       <Box sx={{backgroundColor: "#22272E", display: "flex", alignContent: "center", justifyContent: "center", flexFlow: "column", }}>
