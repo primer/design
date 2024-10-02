57173ce2c5507ac3aeeb8b00b043df2da9da05fe
Lukas Oppermann <lukasoppermann@github.com>
2024-10-02 17:23:13 +0200

    Adding figma image component

---
+++ src/components/figma-image.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {parseFigmaNodeUrl} from '@primer/figma-images/src/utils';
+import { Link, StyledOcticon } from '@primer/react';
+import {PencilIcon} from '@primer/octicons-react'
+
+type FigmaImageProps = React.ImgHTMLAttributes<HTMLImageElement> & {
+  src: string
+  caption?: string
+}
+
+const cssStyles = {
+  width: "100%",
+  maxWidth: "100%",
+  margin: "0",
+  "& img": {
+    width: '100%'
+  }
+}
+
+const FigmaImageDir = '/images/figma';
+
+export const FigmaImage: React.FC<FigmaImageProps> = ({src, caption, ...props}) => {
+  // check for missing prop
+  if(src === undefined) throw new Error("src is required on FigmaImage component");
+  // get real image url
+  const {nodeId, fileId} = parseFigmaNodeUrl(src);
+  const imagePath = `${FigmaImageDir}/${fileId}-${nodeId}.png`
+  // return image component
+  return (<figure className="FigmaImage" style={cssStyles}>
+    <img src={imagePath} {...props}/>
+    <figcaption>
+      {caption}
+       <Link sx={{fontSize: '12px'}} className="link" href={src}><StyledOcticon icon={PencilIcon} sx={{mr: 2}} size="small"/>Edit in Figma</Link>
+    </figcaption>
+  </figure>)
+}
